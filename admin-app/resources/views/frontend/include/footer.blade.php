055e4ff791dcccf52015db3345c707147deaf0be
emranmd <emran01851384660@gmail.com>
2024-05-23 11:39:51 +0600

    e

--- admin-app/resources/views/frontend/include/footer.blade.php
+++ admin-app/resources/views/frontend/include/footer.blade.php
@@ -1,11 +1,12 @@
+<!-- jquery js -->
+<script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
+<!-- bootstrap js -->
+<script src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
 <!-- slick slider js -->
-<script src="{{ asset('slick slider/js/jquery-3.7.1.min.js') }}"></script>
-<script src="{{ asset('slick slider/js/my-slick.js') }}"></script>
-<script src="{{ asset('slick slider/js/slick.min.js') }}"></script>
-
-<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<script src="{{ asset('slick-slider/js/my-slick.js') }}"></script>
+<script src="{{ asset('slick-slider/js/slick.min.js') }}"></script>
+<!-- custom js -->
+<script src="{{ asset('js/custom.js') }}"></script>
 
 </body>
 </html>
--- admin-app/resources/views/frontend/include/header.blade.php
+++ admin-app/resources/views/frontend/include/header.blade.php
@@ -6,19 +6,18 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
     <!-- font-awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-
+    <link rel="stylesheet" href="{{ asset('css/fontAwesome/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fontAwesome/fontawesome.min.css') }}">
     <!-- style.css -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <!-- main.login.css -->
     <link rel="stylesheet" href="{{ asset('css/login/main.login.css') }}">
     <!-- slick slider css -->
-    <link rel="stylesheet" href="{{ asset('slick slider/css/responsive.css') }}">
-    <link rel="stylesheet" href="{{ asset('slick slider/css/slick-theme.css') }}">
-    <link rel="stylesheet" href="{{ asset('slick slider/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('slick-slider/css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('slick-slider/css/slick-theme.css') }}">
+    <link rel="stylesheet" href="{{ asset('slick-slider/css/slick.css') }}">
 
 
 
